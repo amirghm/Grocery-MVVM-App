ea67cbc0a1330cbb412f49db29e15074a2667193
Amir Hossein Ghasemi <amir.ghasemi@sigmasoftwares.com>
2021-04-19 02:19:22 +0300

    feature/implement-hilt-di: - NetworkModule.kt Implemented

--- app/src/main/java/com/amirghm/mobiquitysample/MobiquityApplication.kt
+++ app/src/main/java/com/amirghm/mobiquitysample/MobiquityApplication.kt
@@ -1,14 +1,11 @@
-package com.amirghm.mobiquitysample;
+package com.amirghm.mobiquitysample
 
-import android.app.Application;
+import android.app.Application
 
 /**
  * Created by Amir Hossein Ghasemi since 4/19/2021
- * <p>
  * Usage: Application Main Class.
- * <p>
- * How to call:
  */
-public class MobiquityApplication extends Application {
+class MobiquityApplication : Application(){
 
 }
